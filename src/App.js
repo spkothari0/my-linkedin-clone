9bc9e67d67c6a6f137f956ceb492a61283f72316
Shreyas Pravin Kothari <s.pravin.kothari@accenture.com>
2022-08-30 18:38:31 +0530

    correct redux

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import './App.css'
 import Feed from './components/Feed';
+import { selectUser } from './features/userSlice'
+import { useSelector } from 'react-redux';
 function App() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       {/* {Header} */}
